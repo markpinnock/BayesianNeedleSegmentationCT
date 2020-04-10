b6fb72c3bc56ae93d9bb1a223095862451165fe8
Mark Pinnock <rmappin@ucl.ac.uk>
2020-04-10 10:44:11 +0100

    Altered test image display

--- training/Testing.py
+++ training/Testing.py
@@ -3,6 +3,7 @@ import numpy as np
 import os
 import sys
 import tensorflow as tf
+import tensorflow.keras as keras
 
 sys.path.append('..')
 
@@ -17,8 +18,8 @@ EPOCHS = 500
 ETA = 0.001
 
 FILE_PATH = "C:/Users/roybo/OneDrive - University College London/Collaborations/RobotNeedleSeg/Code/003_CNN_Bayes_Traj/"
-DATA_PATH = "Z:/Robot_Data/Train/"
-EXPT_NAME = f"nc{NC}_ep{EPOCHS}_eta{ETA}"
+DATA_PATH = "Z:/Robot_Data/Test/"
+EXPT_NAME = "spatial_dropout" #f"nc{NC}_ep{EPOCHS}_eta{ETA}" #"nc4_drop0_0"
 MODEL_SAVE_PATH = f"{FILE_PATH}models/{EXPT_NAME}/"
 img_path = f"{DATA_PATH}Img/"
 seg_path = f"{DATA_PATH}Seg/"
@@ -48,13 +49,13 @@ pred_mean = np.zeros((MB_SIZE, 512, 512, 3, 1), dtype=np.float32)
 pred_var = np.zeros((MB_SIZE, 512, 512, 3, 1), dtype=np.float32)
 
 for img, seg in test_ds.batch(MB_SIZE):
-    fig, axs = plt.subplots(MB_SIZE, 7)
     pred = UNetStandard(img, training=False)
     pred_mean[:, :, :, :, 0], pred_var[:, :, :, :, 0], _ = varDropout(img, UNetDropout, T=100)
     temp_metric = diceLoss(pred, seg)
     temp_drop_metric = diceLoss(pred_mean, seg)
     test_metric += temp_metric
-    drop_metric += temp_drop_metric
+    drop_metric += temp_drop_metric # This is probably wrong
+    print(pred.shape, seg.shape, pred_mean.shape)
     print(f"Batch Dice score: {1 - temp_metric / MB_SIZE}, Ensemble Dice score: {1 - temp_drop_metric / MB_SIZE}")
 
     rgb_pred[:, :, :, :, 0] = seg[:, :, :, :, 0]
@@ -64,34 +65,37 @@ for img, seg in test_ds.batch(MB_SIZE):
     rgb_drop[:, :, :, :, 1] = pred_mean[:, :, :, :, 0]
     rgb_drop[:, :, :, :, 2] = pred_mean[:, :, :, :, 0]
 
-    for j in range(img.shape[0]):
-        axs[j, 0].imshow(np.fliplr(img[j, :, :, 1, 0].numpy().T), cmap='gray', vmin=0.12, vmax=0.18, origin='lower')
-        axs[j, 0].axis('off')
-        axs[j, 1].imshow(np.fliplr(img[j, :, :, 1, 0].numpy().T), cmap='gray', vmin=0.12, vmax=0.18, origin='lower')
-        axs[j, 1].axis('off')
-        # axs[j, 1].imshow(np.fliplr(np.ma.masked_where(pred[j, :, :, 1, 0].numpy().T == False, pred[j, :, :, 1, 0].numpy().T)), cmap='Set1', origin='lower')
-        axs[j, 1].imshow(np.fliplr(pred[j, :, :, 1, 0].numpy().T), cmap='hot', alpha=0.3, origin='lower')
-        axs[j, 1].axis('off')
-        axs[j, 2].imshow(np.fliplr(img[j, :, :, 1, 0].numpy().T), cmap='gray', vmin=0.12, vmax=0.18, origin='lower')
-        axs[j, 2].axis('off')
-        # axs[j, 2].imshow(np.fliplr(np.ma.masked_where(pred_mean[j, :, :, 1, 0].T == False, pred[j, :, :, 1, 0].numpy().T)), cmap='Set1', origin='lower')
-        axs[j, 2].imshow(np.fliplr(pred_mean[j, :, :, 1, 0].T), cmap='hot', alpha=0.3, origin='lower')
-        axs[j, 2].axis('off')
-        r_pred = np.fliplr(rgb_pred[j, :, :, 1, 0].T)
-        g_pred = np.fliplr(rgb_pred[j, :, :, 1, 1].T)
-        b_pred = np.fliplr(rgb_pred[j, :, :, 1, 2].T)
-        axs[j, 3].imshow(np.concatenate([r_pred[:, :, np.newaxis], g_pred[:, :, np.newaxis], b_pred[:, :, np.newaxis]], axis=2), origin='lower')
-        axs[j, 3].axis('off')
-        r_drop = np.fliplr(rgb_drop[j, :, :, 1, 0].T)
-        g_drop = np.fliplr(rgb_drop[j, :, :, 1, 1].T)
-        b_drop = np.fliplr(rgb_drop[j, :, :, 1, 2].T)
-        axs[j, 4].imshow(np.concatenate([r_drop[:, :, np.newaxis], g_drop[:, :, np.newaxis], b_drop[:, :, np.newaxis]], axis=2), origin='lower')
-        axs[j, 4].axis('off')
-        axs[j, 5].imshow(np.fliplr(pred_mean[j, :, :, 1, 0].T), cmap='hot', origin='lower')
-        axs[j, 5].axis('off')
-        axs[j, 6].imshow(np.fliplr(pred_var[j, :, :, 1, 0].T), cmap='hot', origin='lower')
-        axs[j, 6].axis('off')
+    for j in range(img.shape[3]):
+        fig, axs = plt.subplots(MB_SIZE, 7)
 
-    plt.show()
+        for i in range(img.shape[0]):
+            axs[i, 0].imshow(np.fliplr(img[i, :, :, j, 0].numpy().T), cmap='gray', vmin=0.12, vmax=0.18, origin='lower')
+            axs[i, 0].axis('off')
+            axs[i, 1].imshow(np.fliplr(img[i, :, :, j, 0].numpy().T), cmap='gray', vmin=0.12, vmax=0.18, origin='lower')
+            axs[i, 1].axis('off')
+            # axs[j, 1].imshow(np.fliplr(np.ma.masked_where(pred[j, :, :, 1, 0].numpy().T == False, pred[j, :, :, 1, 0].numpy().T)), cmap='Set1', origin='lower')
+            axs[i, 1].imshow(np.fliplr(pred[i, :, :, j, 0].numpy().T), cmap='hot', alpha=0.3, origin='lower')
+            axs[i, 1].axis('off')
+            axs[i, 2].imshow(np.fliplr(img[i, :, :, j, 0].numpy().T), cmap='gray', vmin=0.12, vmax=0.18, origin='lower')
+            axs[i, 2].axis('off')
+            # axs[j, 2].imshow(np.fliplr(np.ma.masked_where(pred_mean[j, :, :, 1, 0].T == False, pred[j, :, :, 1, 0].numpy().T)), cmap='Set1', origin='lower')
+            axs[i, 2].imshow(np.fliplr(pred_mean[i, :, :, j, 0].T), cmap='hot', alpha=0.3, origin='lower')
+            axs[i, 2].axis('off')
+            r_pred = np.fliplr(rgb_pred[i, :, :, j, 0].T)
+            g_pred = np.fliplr(rgb_pred[i, :, :, j, 1].T)
+            b_pred = np.fliplr(rgb_pred[i, :, :, j, 2].T)
+            axs[i, 3].imshow(np.concatenate([r_pred[:, :, np.newaxis], g_pred[:, :, np.newaxis], b_pred[:, :, np.newaxis]], axis=2), origin='lower')
+            axs[i, 3].axis('off')
+            r_drop = np.fliplr(rgb_drop[i, :, :, j, 0].T)
+            g_drop = np.fliplr(rgb_drop[i, :, :, j, 1].T)
+            b_drop = np.fliplr(rgb_drop[i, :, :, j, 2].T)
+            axs[i, 4].imshow(np.concatenate([r_drop[:, :, np.newaxis], g_drop[:, :, np.newaxis], b_drop[:, :, np.newaxis]], axis=2), origin='lower')
+            axs[i, 4].axis('off')
+            axs[i, 5].imshow(np.fliplr(pred_mean[i, :, :, j, 0].T), cmap='hot', origin='lower')
+            axs[i, 5].axis('off')
+            axs[i, 6].imshow(np.fliplr(pred_var[i, :, :, j, 0].T), cmap='hot', origin='lower')
+            axs[i, 6].axis('off')
+
+        plt.show()
 
 print(f"Final Dice score: {1 - test_metric / N}, Final ensemble Dice score: {1 - drop_metric / N}")
