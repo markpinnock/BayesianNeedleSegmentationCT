85a8dbcca91030fac0568703812c91b3453ea57c
Mark <mark.pinnock.18@ucl.ac.uk>
2020-10-11 17:26:56 +0100

    Moved test functions to TestFuncs, updated file paths, simplified testing loop and results saving process

--- testing/DiceMAT.py
+++ testing/DiceMAT.py
@@ -1,152 +1,132 @@
+import json
 import numpy as np
 import os
-import scipy.io as io
 import sys
-import tensorflow as tf
 
 sys.path.append('..')
 
-from Networks import UNetGen
-from utils.DataLoader import imgLoader
-from utils.TrainFuncs import diceLoss, haussdorfDistance, NMSCalc, varDropout
+from TestFuncs import thresholdImg, diceLoss, haussdorfDistance, NMSCalc, trajErrorCalc
 
-
-MB_SIZE = 1
-NC = 4
-EPOCHS = 500
-ETA = 0.001
-T = 20
-THRESH = 0.5
-
-FILE_PATH = "C:/Users/roybo/OneDrive - University College London/Collaborations/RobotNeedleSeg/Code/003_CNN_Bayes_Traj/"
 DATA_PATH = "Z:/Robot_Data/Test/"
-SAVE_PATH = "C:/Users/roybo/OneDrive - University College London/Collaborations/RobotNeedleSeg/Code/003_CNN_Bayes_Traj/mat_files/"
-EXPT_NAME = f"nc{NC}_ep{EPOCHS}_eta{ETA}"
-# EXPT_NAME = "spatial_dropout"
-MODEL_SAVE_PATH = f"{FILE_PATH}models/{EXPT_NAME}/"
+NPY_PATH = "C:/Users/roybo/OneDrive - University College London/Collaborations/RobotNeedleSeg/Code/003_CNN_Bayes_Traj/output_npy/"
+FILE_PATH = "C:/Users/roybo/OneDrive - University College London/Collaborations/RobotNeedleSeg/Code/003_CNN_Bayes_Traj/"
 SAVE_PATH = f"{FILE_PATH}mat_files/"
-STD_MODEL_SAVE_PATH = f"{FILE_PATH}models/nc4_drop0_0/nc4_drop0_0"
-START_IDX = 0
 
-img_path = f"{DATA_PATH}Img/"
-seg_path = f"{DATA_PATH}Seg/"
-imgs = os.listdir(img_path)
-segs = os.listdir(seg_path)
-imgs.sort()
+SEG_PATH = f"{DATA_PATH}Seg/"
+segs = os.listdir(SEG_PATH)
 segs.sort()
 
-N = len(imgs)
-assert N == len(segs), "HI/LO IMG PAIRS UNEVEN LENGTHS"
-assert MB_SIZE == 1, "MUST USE MINIBATCH SIZE 1"
+THRESH = 0.5
+T = 20
+STD_PATH = f"{NPY_PATH}std/"
+DROP1_PATH = f"{NPY_PATH}drop1/"
+DROP2_PATH = f"{NPY_PATH}drop2_T{T}/"
+DROP2E_PATH = f"{NPY_PATH}drop2E_T{T}/"
 
-LO_VOL_SIZE = (512, 512, 3, 1, )
-
-test_ds = tf.data.Dataset.from_generator(
-    imgLoader, args=[img_path, seg_path, imgs[START_IDX:], segs[START_IDX:], False, False], output_types=(tf.float32, tf.float32))
-
-UNetStandard = UNetGen(input_shape=LO_VOL_SIZE, starting_channels=NC, drop_rate=0.0, dropout_flag=False)
-UNetStandard.load_weights(f"{STD_MODEL_SAVE_PATH}")
-UNetDropout1 = UNetGen(input_shape=LO_VOL_SIZE, starting_channels=NC, drop_rate=0.0, dropout_flag=False)
-UNetDropout1.load_weights(f"{MODEL_SAVE_PATH}{EXPT_NAME}")
-UNetDropout2 = UNetGen(input_shape=LO_VOL_SIZE, starting_channels=NC, drop_rate=0.2, dropout_flag=True)
-UNetDropout2.load_weights(f"{MODEL_SAVE_PATH}{EXPT_NAME}")
-
-pred_drop_1 = np.zeros((MB_SIZE, 512, 512, 3, 1), dtype=np.float32)
-pred_drop_2 = np.zeros((MB_SIZE, 512, 512, 3, 1), dtype=np.float32)
-pred_entropy = np.zeros((MB_SIZE, 512, 512, 3, 1), dtype=np.float32)
-
-save_count = 0
-idx = 0 + START_IDX
-skip_imgs = ["UCLH_08470270_1_006_L.npy",
-             "UCLH_08470270_1_013_L.npy",
-             "UCLH_08933783_1_007_L.npy",
-             "UCLH_08933783_1_017_L.npy",
-             "UCLH_09099884_1_014_L.npy",
-             "UCLH_09099884_1_023_L.npy",
-             "UCLH_11107604_1_009_L.npy",
-             "UCLH_11107604_1_017_L.npy",
-             "UCLH_11192578_1_008_L.npy",
-             "UCLH_11192578_1_016_L.npy",
-             "UCLH_11349911_1_019_L.npy",
-             "UCLH_11349911_1_027_L.npy"]
+std_list = os.listdir(STD_PATH)
+std_list.sort()
 
 dice_std = []
-dice_drop_1 = []
-dice_drop_2 = []
-dist_std = []
-dist_drop_1 = []
-dist_drop_2 = []
-var_drop_2 = []
-nms_std = []
-nms_drop_1 = []
-nms_drop_2 = []
+dice_drop1 = []
+dice_drop2 = []
+hdf_std = []
+hdf_drop1 = []
+hdf_drop2 = []
+entropy_drop2 = []
+nme_std = []
+nme_drop1 = []
+nme_drop2 = []
+angle_std = []
+angle_drop1 = []
+angle_drop2 = []
+centre_std = []
+centre_drop1 = []
+centre_drop2 = []
+traj_hdf_std = []
+traj_hdf_drop1 = []
+traj_hdf_drop2 = []
 
 
-for img, seg in test_ds.batch(MB_SIZE):
-    pred_std = UNetStandard(img, training=False).numpy()
-    pred_drop_1[:, :, :, :, 0], _, _ = varDropout(img, UNetDropout1, T=1, threshold=None)
-    pred_drop_2[:, :, :, :, 0], pred_entropy[:, :, :, :, 0], _ = varDropout(img, UNetDropout2, T=T, threshold=None)
-    pred_std[pred_std < THRESH] = 0
-    pred_std[pred_std >= THRESH] = 1
-    pred_drop_1[pred_drop_1 < THRESH] = 0
-    pred_drop_1[pred_drop_1 >= THRESH] = 1
-    pred_drop_2[pred_drop_2 < THRESH] = 0
-    pred_drop_2[pred_drop_2 >= THRESH] = 1
+for i in range(len(std_list)):
+    img_stem = std_list[i][:-8]
+    final_seg_list = [seg for seg in segs if img_stem[:-4] in seg]
+    final_seg_list.sort()
+    seg_name = final_seg_list[-1]
+    seg = np.load(f"{SEG_PATH}{seg_name}")
 
-    if imgs[idx] in skip_imgs:
-        print(f"Skipping {imgs[idx]}")
-        idx += 1
-        continue
+    std = np.load(f"{STD_PATH}{img_stem}_STD.npy")
+    drop1 = np.load(f"{DROP1_PATH}{img_stem}_D1.npy")
+    drop2 = np.load(f"{DROP2_PATH}{img_stem}_D2.npy")
+    drop2E = np.load(f"{DROP2E_PATH}{img_stem}_D2E.npy")
 
-    assert MB_SIZE == 1, "INCORRECT MB SIZE"
+    mean_entropy = np.sum(drop2E * drop2) / np.sum(drop2)
 
-    dice_std.append(1 - diceLoss(pred_std, seg).numpy())
-    dice_drop_1.append(1 - diceLoss(pred_drop_1, seg).numpy())
-    dice_drop_2.append(1 - diceLoss(pred_drop_2, seg).numpy())
+    if THRESH != None:
+        std = thresholdImg(std, THRESH)
+        drop1 = thresholdImg(drop1, THRESH)
+        drop2 = thresholdImg(drop2, THRESH)
 
-    if "UCLH_08933783" in imgs[idx]:
+    dice_std.append(1 - diceLoss(std, seg))
+    dice_drop1.append(1 - diceLoss(drop1, seg))
+    dice_drop2.append(1 - diceLoss(drop2, seg))
+
+    if "UCLH_08933783" in img_stem:
         pixel_spacing = 0.976
     else:
         pixel_spacing = 0.781
 
-    dist_std.append(haussdorfDistance(pred_std[:, :, :, 1, :], seg[:, :, :, 1, :], pixel_spacing))
-    dist_drop_1.append(haussdorfDistance(pred_drop_1[:, :, :, 1, :], seg[:, :, :, 1, :], pixel_spacing))
-    dist_drop_2.append(haussdorfDistance(pred_drop_2[:, :, :, 1, :], seg[:, :, :, 1, :], pixel_spacing))
+    hdf_std.append(haussdorfDistance(std, seg, pixel_spacing))
+    hdf_drop1.append(haussdorfDistance(drop1, seg, pixel_spacing))
+    hdf_drop2.append(haussdorfDistance(drop2, seg, pixel_spacing))
 
-    mean_entropy = np.sum(pred_entropy, axis=(1, 2, 3, 4)) / np.sum(np.logical_or(pred_drop_2 > 0.0, pred_entropy > 0.0), axis=(1, 2, 3, 4))
-    # var_drop_2.append(mean_entropy[0, 1])
-    var_drop_2.append(mean_entropy)
-    nms_std.append(NMSCalc(pred_std[0, :, :, 1, 0], THRESH))
-    nms_drop_1.append(NMSCalc(pred_drop_1[0, :, :, 1, 0], THRESH))
-    nms_drop_2.append(NMSCalc(pred_drop_2[0, :, :, 1, 0], THRESH))
+    entropy_drop2.append(mean_entropy)
 
-    # print(
-    #     1 - diceLoss(pred_std, seg).numpy(),
-    #     1 - diceLoss(pred_drop_1, seg).numpy(),
-    #     1 - diceLoss(pred_drop_2, seg).numpy()
-    # )
+    nme_std.append(NMSCalc(std[:, :, 1], THRESH, pixel_spacing))
+    nme_drop1.append(NMSCalc(drop1[:, :, 1], THRESH, pixel_spacing))
+    nme_drop2.append(NMSCalc(drop2[:, :, 1], THRESH, pixel_spacing))
+    temp1 = trajErrorCalc(std[:, :, 1], seg, THRESH, pixel_spacing, False)
+    temp2 = trajErrorCalc(drop1[:, :, 1], seg, THRESH, pixel_spacing, False)
+    temp3 = trajErrorCalc(drop2[:, :, 1], seg, THRESH, pixel_spacing, False)
+    angle_std.append(temp1[0])
+    angle_drop1.append(temp2[0])
+    angle_drop2.append(temp3[0])
+    centre_std.append(temp1[1])
+    centre_drop1.append(temp2[1])
+    centre_drop2.append(temp3[1])
+    traj_hdf_std.append(temp1[2])
+    traj_hdf_drop1.append(temp2[2])
+    traj_hdf_drop2.append(temp3[2])
 
-    print(f"{imgs[idx]}, {NMSCalc(pred_drop_2[0, :, :, 1, 0], THRESH)}")
+    # print(f"{imgs[idx]}, {NMSCalc(drop2[:, :, 1], THRESH)}")
 
-    # print(
-    #     haussdorfDistance(pred_std[:, :, :, 1, :], seg[:, :, :, 1, :]),
-    #     haussdorfDistance(pred_drop_1[:, :, :, 1, :], seg[:, :, :, 1, :]),
-    #     haussdorfDistance(pred_drop_2[:, :, :, 1, :], seg[:, :, :, 1, :])
-    # )
-
-    idx += 1
-
+# print(np.sqrt(np.nanmean(np.square(traj_std))), np.sqrt(np.nanmean(np.square(centre_std))), np.nanmean(traj_hdf_std))
+# print(np.sqrt(np.mean(np.square(traj_drop1))), np.sqrt(np.mean(np.square(centre_drop1))), np.mean(traj_hdf_drop1))
+# print(np.sqrt(np.mean(np.square(traj_drop2))), np.sqrt(np.mean(np.square(centre_drop2))), np.mean(traj_hdf_drop2))
+#
+# print("========================")
+# print(np.mean(dice_std), np.mean(dice_drop1), np.mean(dice_drop2))
+# print(np.nanmean(nme_std), np.mean(nme_drop1), np.mean(nme_drop2))
 list_dict = {
     'dice_std': dice_std,
-    'dice_drop_1': dice_drop_1,
-    'dice_drop_2': dice_drop_2,
-    'dist_std': dist_std,
-    'dist_drop_1': dist_drop_1,
-    'dist_drop_2': dist_drop_2,
-    'var_drop_2': var_drop_2,
-    'nms_std': nms_std,
-    'nms_drop_1': nms_drop_1,
-    'nms_drop_2': nms_drop_2
+    'dice_drop1': dice_drop1,
+    'dice_drop2': dice_drop2,
+    'hdf_std': hdf_std,
+    'hdf_drop1': hdf_drop1,
+    'hdf_drop2': hdf_drop2,
+    'entropy_drop2': entropy_drop2,
+    'nme_std': nme_std,
+    'nme_drop1': nme_drop1,
+    'nme_drop2': nme_drop2,
+    'angle_std': angle_std,
+    'angle_drop1': angle_drop1,
+    'angle_drop2': angle_drop2,
+    'centre_std': centre_std,
+    'centre_drop1': centre_drop1,
+    'centre_drop2': centre_drop2,
+    'traj_hdf_std': traj_hdf_std,
+    'traj_hdf_drop1': traj_hdf_drop1,
+    'traj_hdf_drop2':traj_hdf_drop2
 }
 
-io.savemat(f"{SAVE_PATH}dice_var4.mat", list_dict)
+with open(f"{SAVE_PATH}results05.json", 'w') as out_file:
+    json.dump(list_dict, out_file, indent=4)
