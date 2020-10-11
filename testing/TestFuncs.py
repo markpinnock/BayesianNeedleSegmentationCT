ae2f9efb73636999601991b86b7aec7e4f930322
Mark <mark.pinnock.18@ucl.ac.uk>
2020-10-11 17:31:30 +0100

    Simplified Dice score, modified NMS func to account for pixel spacing, modified histograms, added trajectory error function

--- testing/TestFuncs.py
+++ testing/TestFuncs.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import numpy as np
 
 
@@ -16,23 +17,24 @@ def calcMeanEntropy(img_pred, img_entropy):
 
 
 def diceLoss(pred, mask):
-    numer = np.sum(pred * mask, axis=[1, 2, 3, 4]) * 2
-    denom = np.sum(pred, axis=[1, 2, 3, 4]) + np.sum(mask, axis=[1, 2, 3, 4]) + 1e-6
+    numer = np.sum(pred * mask) * 2
+    denom = np.sum(pred) + np.sum(mask) + 1e-6
     dice = numer / denom
 
-    return 1 - np.mean(dice)
+    return 1 - dice
 
 
 def haussdorfDistance(pred, mask, pix_sp):
+
     def dist(a, b, pix_sp):
         d = (a - b) * pix_sp
         return np.sqrt(d @ d.T)
 
-    X = np.argwhere(pred[0, :, :, 0] == 1)
-    Y = np.argwhere(mask[0, :, :, 0] == 1)
+    X = np.argwhere(pred[:, :, 1] == 1)
+    Y = np.argwhere(mask[:, :, 1] == 1)
 
     if X.sum() == 0:
-        return 1000
+        return np.nan
 
     d1 = max(min(dist(X[i, :], Y[j, :], pix_sp) for i in range(X.shape[0])) for j in range(Y.shape[0]))
     d2 = max(min(dist(X[i, :], Y[j, :], pix_sp) for j in range(Y.shape[0])) for i in range(X.shape[0]))
@@ -40,7 +42,7 @@ def haussdorfDistance(pred, mask, pix_sp):
     return max(d1, d2)
 
 
-def NMSCalc(pred, threshold, test=False):
+def NMSCalc(pred, threshold, pix_sp, test=False):
     pred[pred < threshold] == 0
     pred[pred >= threshold] == 1
     pred = pred
@@ -64,6 +66,7 @@ def NMSCalc(pred, threshold, test=False):
     unit_vec = np.array([[xb[1] - xb[0]], [yb[1] - yb[0]]])
     unit_vec /= np.sqrt(unit_vec.T @ unit_vec)
     err_vec = np.concatenate([np.zeros(err.T.shape), err.T], axis=0)
+    err_vec *= pix_sp
     proju = (unit_vec.T @ err_vec) * unit_vec
     projv = err_vec - proju
     dist = np.sqrt(np.sum(projv * projv, axis=0))
@@ -73,11 +76,12 @@ def NMSCalc(pred, threshold, test=False):
         test_err = err_vec[:, 0::test_size]
         test_proju = proju[:, 0::test_size]
         test_projv = projv[:, 0::test_size]
+
         test_x = x[0::test_size]
         test_y = y[0::test_size]
 
         plt.figure()
-        plt.subplot(1, 3, 1)
+        plt.subplot(2, 2, 1)
         plt.imshow(pred, cmap='gray', origin='lower')
         plt.plot(xb, yb, 'r-')
 
@@ -95,16 +99,86 @@ def NMSCalc(pred, threshold, test=False):
             )
         plt.axis('square')
 
-        plt.subplot(1, 3, 2)
-        plt.hist(err, bins=20)
-        plt.axis('square')
-        plt.title(f"Error: {err.std()}")
-
-        plt.subplot(1, 3, 3)
+        plt.subplot(2, 2, 2)
         plt.hist(dist, bins=20)
         plt.axis('square')
-        plt.title(f"Dist: {dist.std()}")
+        plt.title(f" Proj error: {(dist.T @ dist) / dist.shape[0]}")
+
+        plt.subplot(2, 2, 3)
+        plt.hist(projv[0, :], bins=20)
+        plt.axis('square')
+        plt.title(f"x error: {(projv[0, :].T @ projv[0, :]) / projv.shape[1]}")
+
+        plt.subplot(2, 2, 4)
+        plt.hist(projv[1, :], bins=20)
+        plt.axis('square')
+        plt.title(f" y error: {(projv[1, :].T @ projv[1, :]) / projv.shape[1]}")
 
         plt.show()
 
-    return dist.std()
+    assert np.isclose(((projv[0, :].T @ projv[0, :]) + (projv[1, :].T @ projv[1, :])) / projv.shape[1] - ((dist.T @ dist)) / dist.shape[0], 0.0)
+
+    return (dist.T @ dist) / dist.shape[0]
+
+def trajErrorCalc(pred, mask, threshold, pix_sp, test=False):
+
+    def dist(a, b, pix_sp):
+        d = (a - b) * pix_sp
+        return np.sqrt(d @ d.T)
+
+    pred[pred < threshold] == 0
+    pred[pred >= threshold] == 1
+
+    temp = np.argwhere(pred == 1.0)
+    xp = temp[:, 1][:, np.newaxis]
+    yp = temp[:, 0][:, np.newaxis]
+    Xp = np.concatenate([np.ones(xp.shape), xp], axis=1)
+
+    try:
+        Xpinv = np.linalg.inv(Xp.T @ Xp) @ Xp.T
+    except np.linalg.LinAlgError:
+        print(Xp.T @ Xp)
+        return (np.nan, np.nan, np.nan)
+
+    betap = Xpinv @ yp
+    xb = np.linspace(0, 511, 512)
+    ybp = betap[0] + betap[1] * xb
+
+    temp = np.argwhere(mask == 1.0)
+    xg = temp[:, 1][:, np.newaxis]
+    yg = temp[:, 0][:, np.newaxis]
+    Xg = np.concatenate([np.ones(xg.shape), xg], axis=1)
+    Xginv = np.linalg.inv(Xg.T @ Xg) @ Xg.T
+    betag = Xginv @ yg
+    ybg = betag[0] + betag[1] * xb
+
+    anglep = np.arctan((xb[-1] - xb[0]) / (ybp[-1] - ybp[0])) / np.pi * 180
+    angleg = np.arctan((xb[-1] - xb[0]) / (ybg[-1] - ybg[0])) / np.pi * 180
+
+    xbt = xb - 255.5
+    ybgt = ybg - 255.5
+    ybpt = ybp - 255.5
+
+    dgt = np.sqrt(xbt ** 2 + ybgt ** 2)
+    dpt = np.sqrt(xbt ** 2 + ybpt ** 2)
+    idxg = np.argmin(dgt)
+    idxp = np.argmin(dpt)
+
+    A = np.vstack([xb, ybp]).T
+    B = np.vstack([xb, ybg]).T
+
+    d1 = max(min(dist(A[i, :], B[j, :], pix_sp) for i in range(A.shape[0])) for j in range(B.shape[0]))
+    d2 = max(min(dist(A[i, :], B[j, :], pix_sp) for j in range(B.shape[0])) for i in range(A.shape[0]))
+
+    if test:
+        print(anglep, angleg, anglep - angleg, dpt[idxp] - dgt[idxg], max(d1, d2))
+        plt.figure()
+        plt.imshow(pred, cmap='gray', origin='lower')
+        plt.plot(xb, ybp, 'r-')
+        plt.plot(xb, ybg, 'y-')
+        plt.plot(xb[idxg], ybg[idxg], 'g+')
+        plt.plot(xb[idxp], ybp[idxp], 'g+')
+        plt.plot(255.5, 255.5, 'w+')
+        plt.show()
+
+    return (anglep - angleg, dpt[idxp] - dgt[idxg], max(d1, d2))
